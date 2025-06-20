2cfdd8797191b59457e4a26dd71dd521f9e6dfce
llatifah99 <litalatifah6@gmail.com>
2025-06-20 12:32:11 +0700

    start page

--- quiz/lib/start_screen.dart
+++ quiz/lib/start_screen.dart
@@ -5,6 +5,28 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Hello"));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min, // space sesuai kebutuhan children
+        children: [
+          Image.asset('assets/images/logo.png', width: 300),
+          SizedBox(height: 10),
+          Text(
+            "Learn Math in Fun way",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: Text("Start Quiz"),
+          ),
+        ],
+      ),
+    );
   }
 }
