a6e3eb29d816a879b1bfbd53a76bb84d3f337024
llatifah99 <litalatifah6@gmail.com>
2025-06-20 11:57:17 +0700

    bg-color linier

---
+++ quiz/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 225, 73, 236),
+                const Color.fromARGB(255, 85, 11, 82),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
